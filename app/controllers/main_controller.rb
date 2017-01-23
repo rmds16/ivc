f974c2cb3072d347d140be68d197a4a80ff736c1
Richard da Silva <richard@localhost>
2017-01-23 13:31:31 +0000

    Removed pre cancancan authorization functions

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,4 @@
 class MainController < ApplicationController
-  # before_filter :require_no_user, :only => [:new, :create]
-  # before_filter :require_user, :only => :destroy
   
   def calendar
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
-  before_filter :require_no_user, :only => [:new, :create]
-  before_filter :require_user, :only => [:show, :edit, :update]
   load_and_authorize_resource
   
   def new
