62e08caa84a05528d16075f923aacc93160d1ad8
Richard da Silva <richard@localhost>
2017-01-31 15:36:24 +0000

    Admin user section layout updates and added user creation

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,8 +1,12 @@
 class Admin::UsersController < AdminController
   load_and_authorize_resource :user
 
+  def new
+    @user = User.new
+  end
+
   def index
-    @users = User.paginate(page: params[:page], per_page: 10)
+    @users = User.order(:surname, :first_name).paginate(page: params[:page], per_page: 10)
   end
 
   def edit
@@ -11,11 +15,27 @@ class Admin::UsersController < AdminController
 
   def update
     @user = User.find_by(id: params[:id])
-    if @user.update_attributes(params[:user].permit!)
+    if @user.update_attributes(user_params)
       flash[:notice] = "Account updated!"
       redirect_to admin_users_path
     else
       render action: :edit
     end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save!
+      flash[:notice] = "Account registered!"
+      redirect_back_or_default admin_users_path
+    else
+      render action: :new
+    end
+  end
+
+  private
+ 
+  def user_params
+    params.require(:user).permit(:first_name, :surname, :email, :password, :password_confirmation, :admin)
   end
 end
