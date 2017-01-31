fbe0b7d6cd1ff7dcb91efcc5868e6e7a3abfa73b
Richard da Silva <richard@localhost>
2017-01-31 16:09:16 +0000

    Removed actions no longer needed within my account events

--- app/controllers/my_account/events_controller.rb
+++ app/controllers/my_account/events_controller.rb
@@ -4,15 +4,4 @@ class MyAccount::EventsController < MyAccountController
     @events = current_user.attended_events.paginate(page: params[:page], per_page: 10)
   end
 
-  def show
-    @event = Event.find params[:id]
-  end
-
-  def destroy
-    event = Event.find params[:id]
-    if event && current_user.attended_events.delete(event)
-      flash[:notice] = "Event removed!"
-    end
-    redirect_to my_account_events_path
-  end
 end
