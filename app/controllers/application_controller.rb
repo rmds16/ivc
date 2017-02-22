f6c0ecce476c15a4a150c9a8b81eb21ad06570de
Richard da Silva <richard@localhost>
2017-02-22 19:23:44 +0000

    Rescue cancan authorisation errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:danger] = exception.message
+    redirect_to current_user ? calendar_path : signin_path
+  end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
