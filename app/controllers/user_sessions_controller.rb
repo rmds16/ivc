82427441598eb64bbedbd833f1fff3fec53e995e
Richard da Silva <richard@da-silva.me.uk>
2018-12-30 13:03:09 +0000

    Updated authlogic changes

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -33,6 +33,6 @@ class UserSessionsController < ApplicationController
   private
 
   def user_session_params
-    params.require(:user_session).permit(:email, :password, :remember_me)
+    params.require(:user_session).permit(:email, :password, :remember_me).to_h
   end
 end
